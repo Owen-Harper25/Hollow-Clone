73b152bc613a43fcffef193eef6c8628c641cc71
Wafflexan <101963408+Wafflexan@users.noreply.github.com>
2025-09-18 23:00:56 -0700

    New TileSet

--- Scripts/title_screen_gui.gd
+++ Scripts/title_screen_gui.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_game_button_down() -> void:
-	get_tree().change_scene_to_file("res://Scenes/testmap.tscn")
+	get_tree().change_scene_to_file("res://Scenes/map_1.tscn")
 	pass # Replace with function body.
 
 func _on_quit_game_button_down() -> void:
