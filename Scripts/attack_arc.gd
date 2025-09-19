853acfc415200d45eb6c6200d3dd75b4e8f95678
Owen-Harper25 <pistongames25@gmail.com>
2025-09-18 23:02:20 -0700

    Working on attack

---
+++ Scripts/attack_arc.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var attack_damage: int = 10  # Damage value
+@export var damage: int = 1  # Attack damage
+@onready var timer: Timer = $Timer  # Reference to the timer
+
+func _ready():
+	timer.start()  # Start lifespan timer
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
+
+func _on_attack_arc_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy") and body.has_method("take_damage"):
+		body.take_damage(damage)
