99ae19b159f8fbbfdc71021841f55695fc8d1114
Wafflexan <101963408+Wafflexan@users.noreply.github.com>
2025-09-18 18:44:49 -0700

    Title Screen
    A totally complete title screen

---
+++ Scenes/title_screen_gui.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _on_play_game_button_down() -> void:
+	get_tree().change_scene_to_file("res://Scenes/testmap.tscn")
+	pass # Replace with function body.
+
+func _on_quit_game_button_down() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
