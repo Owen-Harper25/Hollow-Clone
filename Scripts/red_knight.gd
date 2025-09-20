897e61f40da993f62a94d9f51082616e6bd3ce99
Wafflexan <101963408+Wafflexan@users.noreply.github.com>
2025-09-20 16:55:03 -0700

    Beginning of enemy

---
+++ Scripts/red_knight.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+class_name Red_Knight
+
+const speed = 10
+var is_redknight_chase: bool
+	
+
+@export var health = 5
+@export var max_health = 5
+var health_min = 0
+
+var dead: bool = false
+var taking_damage: bool = false
+@export var damage_to_deal = 1
+var is_dealing_damage: bool = false
+
+var dir: Vector2
+const gravity = 300
+var knockback_force = 200
+var is_roaming: bool = false
+
+func _process(delta: float) -> void:
+	if !is_on_floor():
+		velocity.y += gravity * delta
+		velocity.x = 0
+	move(delta)
+	move_and_slide()
+
+func move(delta):
+	if !dead:
+		if !is_redknight_chase:
+			velocity += dir * speed * delta
+		is_roaming = true
+	elif dead:
+		velocity.x = 0
+
+func _on_direction_timer_timeout() -> void:
+	$DirectionTimer.wait_time = choose([1.5, 2.0, 2.5])
+	if !is_redknight_chase: 
+		dir = choose([Vector2.RIGHT, Vector2.LEFT])
+		velocity.x = 0
+	pass # Replace with function body.
+
+func choose(array):
+	array.shuffle()
+	return array.front()
