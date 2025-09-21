946a20c6189a96d90c1cdf9dfa39360f5ad70fd8
Wafflexan <101963408+Wafflexan@users.noreply.github.com>
2025-09-20 17:51:37 -0700

    Random Changes

--- Scripts/attack_arc.gd
+++ Scripts/attack_arc.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-@export var attack_damage: int = 10  # Damage value
-@export var damage: int = 1  # Attack damage
+@export var damage: int = 1  # damage
 @onready var timer: Timer = $Timer  # Reference to the timer
 
 func _ready():
@@ -12,5 +11,5 @@ func _on_timer_timeout() -> void:
 	queue_free()
 
 func _on_attack_arc_area_body_entered(body: Node2D) -> void:
-	if body.is_in_group("enemy") and body.has_method("take_damage"):
+	if body.is_in_group("Enemy") and body.has_method("take_damage"):
 		body.take_damage(damage)
