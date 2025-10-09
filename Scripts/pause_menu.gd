c2b4f6fa4003ac92e41b1a4032c89383f37fd521
Wafflexan <101963408+Wafflexan@users.noreply.github.com>
2025-10-08 20:58:40 -0700

    UI UPDATE

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -2,12 +2,16 @@ extends Control
 
 func _ready() -> void:
 	hide()
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
 
 func _process(_delta: float) -> void:
 	escapeTest()
 
 func resume():
 	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	hide()
 	$AnimationPlayer.play_backwards("blur")
 	Global.emit_signal("game_resumed")
@@ -15,9 +19,10 @@ func resume():
 
 func pause():
 	get_tree().paused = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	show()
 	$AnimationPlayer.play("blur")
-	$PanelContainer/VBoxContainer/Resume.grab_focus()
+	$PanelContainer/MarginContainer/VBoxContainer/Resume.grab_focus()
 
 func escapeTest():
 	if Input.is_action_just_pressed("escape") and !get_tree().paused:
