8a433724bf15567ee2505073fa08f35eb13b8b17
Owen-Harper25 <pistongames25@gmail.com>
2025-10-14 14:08:13 -0700

    Coin Pile Juice

--- Scripts/currency_pile.gd
+++ Scripts/currency_pile.gd
@@ -5,34 +5,29 @@ var broken: bool = false
 @export var object_health: int = 0
 var minimum_object_health: int = 0
 @onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var coin = preload("res://Scenes/Enviromentals/coin.tscn")
-
+@onready var coin_scene = preload("res://Scenes/Enviromentals/coin.tscn")
+@export var coins_per_hit: int = 3
+@export var coin_spread_angle: float = PI / 3
+@export var coin_min_speed: float = 20.0
+@export var coin_max_speed: float = 70.0
 
 func break_dmg(attack: Attack):
-	if object_health > minimum_object_health and sprite_2d.frame == 1:
+	if object_health > minimum_object_health and sprite_2d.frame < 3:
 		taking_break_dmg = true
 		object_health -= attack.attack_dmg
 		print(object_health)
 		sprite_2d.frame += 1
-		print("Spawn Currency")
-		var instance = coin.instantiate()
-		instance.position = position
-		get_tree().current_scene.call_deferred("add_child", instance)
-	elif object_health > minimum_object_health and sprite_2d.frame == 2:
-		taking_break_dmg = true
-		object_health -= attack.attack_dmg
-		print(object_health)
-		sprite_2d.frame += 1
-		print("Spawn Currency")
-		var instance = coin.instantiate()
-		instance.position = position
-		get_tree().current_scene.call_deferred("add_child", instance)
+		
+		for i in range(coins_per_hit):
+			var instance = coin_scene.instantiate()
+			instance.position = global_position
+			var angle = randf_range(-coin_spread_angle/2, coin_spread_angle/2)
+			var direction = Vector2(cos(angle), sin(angle))
+			var speed = randf_range(coin_min_speed, coin_max_speed)
+			
+			if instance is RigidBody2D:
+				instance.linear_velocity = direction * speed
+			get_tree().current_scene.call_deferred("add_child", instance)
+			
 	elif object_health <= minimum_object_health:
 		broken = true
-	#elif object_health <= minimum_object_health:
-		#object_health = minimum_object_health
-		#broken = true
-		#$AnimatedSprite2D.play("Break")
-		#SoundLibrary.play_random_break()
-		#await $AnimatedSprite2D.animation_finished
-		#queue_free()
