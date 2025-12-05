18029d9927c7d02671b5d7d686919d608df58dbf
Owen-Harper25 <pistongames25@gmail.com>
2025-12-04 16:33:08 -0800

    Outline fix

--- Scripts/interactable.gd
+++ Scripts/interactable.gd
@@ -3,9 +3,15 @@ extends Area2D
 @export var interact_name: String = ""
 @export var is_interactable: bool = true
 @onready var player = get_tree().get_root().find_child("Player", true, false)
-@onready var sprite: Sprite2D = get_parent().get_node_or_null("Sprite2D")
 
 var interact: Callable = func(): pass
+var sprite: CanvasItem = null
+
+func _ready() -> void:
+	sprite = get_parent().get_node_or_null("Sprite2D")
+
+	if sprite == null:
+		sprite = get_parent().get_node_or_null("AnimatedSprite2D")
 
 func set_highlighted(state: bool) -> void:
 	if sprite and sprite.material:
